bb4226273c6c74c6cb1831ca8a983861efd4b3ce
sdpa <sdpa14@gmail.com>
2020-02-26 19:54:03 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
 import pyautogui
 import cv2
-from random import randint
-import math
+import random
 import numpy as np
+import math
+from bresenham import bresenham
+import os
 
-"""
-This program goes into a infinite Loop !!! Need to stopped at user discretion. 
-"""
 def find_controls(template, source_image):
     source_image = cv2.imread(source_image, 0)  # Reads image in color
     template = cv2.imread(template, 0)
@@ -23,6 +22,15 @@ def find_controls(template, source_image):
     center = (int((top_left[0] + bottom_right[0]) / 2), int((top_left[1] + bottom_right[1]) / 2))
     return center
 
+
+
+def get_RGB_from_Image(point,img):
+    x = point[0]
+    y = point[1]
+    # pixel_b, pixel_g, pixel_r = image[row][column]
+    RGB_from_image = {'red': img.item(y,x,2), 'green':img.item(y,x,1),'blue':img.item(y,x,0)}
+    return RGB_from_image, x, y
+
 def edit_RGB_values (edit_colors,RGB_locations,okLocation,RGB):
     # print(RGB_locations[key]) #<--- this points to the insert locations
     pyautogui.moveTo(edit_colors)
@@ -35,43 +43,44 @@ def edit_RGB_values (edit_colors,RGB_locations,okLocation,RGB):
     pyautogui.moveTo(okLocation)
     pyautogui.click()
 
-def get_RGB_from_Image(imgHei, imgWid):
-    x = randint(0, imgWid-1)
-    y = randint(0, imgHei-1)
-    # pixel_b, pixel_g, pixel_r = image[row][column]
-    RGB_from_image = {'red': mainImg.item(y,x,2), 'green':mainImg.item(y,x,1),'blue':mainImg.item(y,x,0)}
-    return RGB_from_image, x, y
-
 def drawlines(x1, y1):
+    pixelsDrawn = []
     r = 10
-    x1 = start_point[0] + x1
-    y1 = start_point[1] + y1
-
+    x1 = int(start_point[0] + x1)
+    y1 = int(start_point[1] + y1)
     # Use parametric equations
-    t_degree = randint(0,360)
+    t_degree = random.randint(0,360)
     t_radians = math.radians(t_degree)
-    x2 = x1 + r*(math.cos(t_radians))
-    y2 = y1 + r*(math.sin(t_radians))
+    x2 = x1 + int(r*(math.cos(t_radians)))
+    y2 = y1 + int(r*(math.sin(t_radians)))
     pyautogui.moveTo(x1, y1)
     pyautogui.dragTo(x2, y2, button='left')
+    pixelsDrawn = pixelsDrawn + (list(bresenham(x1, y1, x2, y2)))
 
     stepsize = 2  # moves over 2 pixels
     miniRsum = 0
     while(miniRsum < r):
-        x1 = x1 + stepsize*(math.cos(t_radians+(math.pi/2)))
-        y1 = y1 + stepsize*(math.sin(t_radians+(math.pi/2)))
-        x2 = x1 + r * (math.cos(t_radians))
-        y2 = y1 + r * (math.sin(t_radians))
+        x1 = x1 + int(stepsize*(math.cos(t_radians+(math.pi/2))))
+        y1 = y1 + int(stepsize*(math.sin(t_radians+(math.pi/2))))
+        x2 = x1 + int(r * (math.cos(t_radians)))
+        y2 = y1 + int(r * (math.sin(t_radians)))
         miniRsum = miniRsum + stepsize
         pyautogui.moveTo(x1, y1)
         pyautogui.dragTo(x2, y2, button='left')
-    return 0
+        pixelsDrawn = pixelsDrawn + (list(bresenham(x1, y1, x2, y2)))
+    no_duplicate_list = []
+    #Removes all duplicates in the list.
+    for x in pixelsDrawn:
+        if x not in no_duplicate_list:
+            no_duplicate_list.append(x)
+    #[no_duplicate_list.append(x) for x in pixelsDrawn if x not in no_duplicate_list]
+    return no_duplicate_list
 
 if __name__== "__main__":
     # Calibrate  #Height = img.shape[0] , Width = img.shape[1]
     mainImg = cv2.imread('landscape.jpg', cv2.IMREAD_COLOR)
-    maxWidth = 500
-    maxHeight = 500
+    maxWidth = 800
+    maxHeight = 600
     scaling_factors = np.arange(1, 0, -0.001).tolist()
     for i in scaling_factors:
         if mainImg.shape[1] > maxWidth or mainImg.shape[0] > maxHeight:
@@ -88,11 +97,25 @@ if __name__== "__main__":
                      'blue':find_controls('blue.PNG', 'edit_colors_window.png')}
     ok_button = find_controls('ok.png', 'edit_colors_window.png')
 
-    while True:
-        RGBFromImage, randomX, randomY = get_RGB_from_Image(imageHeight, imageWidth)
+    chooselist = [] #Creates a list of pixels from the image dimensions
+    for i in range(0,imageWidth):
+        for j in range(0,imageHeight):
+            chooselist.append((i,j))
+    #print("Array created: \n", chooselist)
+
+    while(len(chooselist) > 0):
+        randomPoint = random.choice(chooselist)
+        RGBFromImage, randomX, randomY = get_RGB_from_Image(randomPoint,mainImg)
         edit_RGB_values(edit_colors, RGB_locations, ok_button, RGBFromImage)
-        drawlines(randomX, randomY)
+        pixelsToDelete = drawlines(randomX, randomY)
 
-
+        #Normalizes points back to image pixels.
+        for i in range(len(pixelsToDelete)):
+            #Performs tuple list substraction.
+            pixelsToDelete[i] = tuple(np.subtract((pixelsToDelete[i][0],pixelsToDelete[i][1]),(54,261)))
+        #This removes drawn pixels from the origial list.
+        for pixel in pixelsToDelete:
+            if(pixel in chooselist):
+                chooselist.remove(pixel)
 
 
